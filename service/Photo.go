3886ffa9ef059edde76d756d723dd885d67c6fe6
CookieNyanCloud <emil8yunusov@gmail.com>
2021-09-13 00:24:53 +0300

    rest

---
+++ service/Photo.go
@@ -0,0 +1,62 @@
+package service
+
+import (
+	"fmt"
+	"google.golang.org/api/drive/v3"
+	"io"
+	"log"
+	"os"
+)
+
+func GetPhoto(srv *drive.Service, name string) ([]string, error) {
+
+	query := `name contains '` + name + "'"
+	r, err := srv.Files.
+		List().
+		PageSize(20).
+		Fields("nextPageToken, files(id, name)").
+		IncludeItemsFromAllDrives(true).
+		SupportsAllDrives(true).
+		Q(query).
+		IncludePermissionsForView("published").
+		Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve files: %v", err)
+	}
+	fmt.Println("Files:")
+	println(len(r.Files))
+	fileslist:= make([]string, len(r.Files))
+	if len(r.Files) == 0 {
+		fmt.Println("No files found.")
+	} else {
+		for j, i := range r.Files {
+			fmt.Printf("%s (%s))\n", i.Name, i.Id)
+			//fileslist = append(fileslist,i.Name)
+			fileslist[j]= i.Name
+			err = load(srv, i)
+		}
+	}
+	return fileslist,nil
+}
+
+func load(srv *drive.Service, r *drive.File) error {
+	res, err := srv.Files.Get(r.Id).Download()
+
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return err
+	}
+	fileNew, err := os.Create(r.Name)
+	if err != nil {
+		return err
+	}
+	defer fileNew.Close()
+	_, err = io.Copy(fileNew, res.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
